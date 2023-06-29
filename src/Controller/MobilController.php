89aab1047460c540b3135c1d8dc3958569b473fe
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-06-29 17:35:56 +0200

    search bar ok

--- src/Controller/MobilController.php
+++ src/Controller/MobilController.php
@@ -9,15 +9,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\SearchSmartphoneType;
 
 #[Route('/mobil')]
 class MobilController extends AbstractController
 {
     #[Route('/', name: 'app_mobil_index', methods: ['GET'])]
-    public function index(MobilRepository $mobilRepository): Response
+    public function index(Request $request, MobilRepository $mobilRepository): Response
     {
+        $form = $this->createForm(SearchSmartphoneType::class);
+        $form->handleRequest($request);
+
+        $mobils = [];
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+
+            $mobils = $mobilRepository->findMobil($search, $mobils);
+        } else {
+            $mobils = $mobilRepository->findAll();
+        }
+
         return $this->render('mobil/index.html.twig', [
             'mobils' => $mobilRepository->findAll(),
+            'form' => $form->createView(),
+
         ]);
     }
 
@@ -34,7 +49,7 @@ class MobilController extends AbstractController
             return $this->redirectToRoute('app_mobil_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('mobil/new.html.twig', [
+        return $this->render('mobil/new.html.twig', [
             'mobil' => $mobil,
             'form' => $form,
         ]);
@@ -60,7 +75,7 @@ class MobilController extends AbstractController
             return $this->redirectToRoute('app_mobil_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('mobil/edit.html.twig', [
+        return $this->render('mobil/edit.html.twig', [
             'mobil' => $mobil,
             'form' => $form,
         ]);
@@ -69,10 +84,46 @@ class MobilController extends AbstractController
     #[Route('/{id}', name: 'app_mobil_delete', methods: ['POST'])]
     public function delete(Request $request, Mobil $mobil, MobilRepository $mobilRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$mobil->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $mobil->getId(), $request->request->get('_token'))) {
             $mobilRepository->remove($mobil, true);
         }
 
         return $this->redirectToRoute('app_mobil_index', [], Response::HTTP_SEE_OTHER);
     }
 }
+
+// namespace App\Controller;
+
+// use App\Repository\CharacteristicRepository;
+// use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Response;
+// use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+// use Symfony\Component\Form\Extension\Core\Type\SearchType;
+
+
+// class AccueilController extends AbstractController
+// {
+//     #[Route('/home', name: 'app_accueil')]
+//     public function index(
+//         Request $request,
+//         CharacteristicRepository $characteristicRepository,
+//     ): Response {
+//         $form = $this->createForm(SearchSmartphoneType::class);
+//         $form->handleRequest($request);
+
+//         $characteristics = [];
+//         if ($form->isSubmitted() && $form->isValid()) {
+//             $search = $form->getData()['search'];
+
+//             $characteristics = $characteristicRepository->findCharacteristic($search);
+//         } else {
+//             $characteristics = $characteristicRepository->findAll();
+//         }
+
+//         return $this->render('accueil/index.html.twig', [
+//             'characteristics' => $characteristics,
+//             'form' => $form->createView(),
+//         ]);
+//     }
+// }
---
+++ src/Form/SearchSmartphoneType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Mobil;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+
+class SearchSmartphoneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->setMethod('GET')
+            ->add('search', SearchType::class)
+            ->add('characteristic', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'RAM',
+                'placeholder' => 'Mémoire',
+            ])
+            ->add('marque', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'marque',
+                'placeholder' => 'Marque',
+            ])
+            ->add('modele', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'modele',
+                'placeholder' => 'Modéle',
+            ])
+            ->add('reseau', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'reseau',
+                'placeholder' => 'Réseau',
+            ])
+            ->add('ecran', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'ecran',
+                'placeholder' => 'Écran',
+            ])
+            ->add('stockage', EntityType::class, [
+                'class' => Mobil::class,
+                'choice_label' => 'stockage',
+                'placeholder' => 'Stockage',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+
+            'csrf_protection' => false,
+        ]);
+    }
+
+    public function getBlockPrefix()
+    {
+        return '';
+    }
+}
