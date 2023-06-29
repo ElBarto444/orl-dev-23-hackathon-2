952c3df273e8db8b214ac964d8f65fc323b734a3
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-06-29 12:27:15 +0200

    created calculator

---
+++ migrations/Version20230629102327.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230629102327 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE calculator (id INT AUTO_INCREMENT NOT NULL, reseau VARCHAR(255) NOT NULL, stockage INT NOT NULL, ecran VARCHAR(255) NOT NULL, ram VARCHAR(255) NOT NULL, phone_condition VARCHAR(255) NOT NULL, category VARCHAR(255) DEFAULT NULL, memory_val INT DEFAULT NULL, storage_value INT DEFAULT NULL, pricing_percentage INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE calculator');
+    }
+}
