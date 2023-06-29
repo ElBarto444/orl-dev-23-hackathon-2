373b1048f19f0cf20e4582489ae2d95b4799872a
mathysfrc <mathysf41@gmail.com>
2023-06-29 17:21:35 +0200

    modifs

--- src/Controller/AdminPageController.php
+++ src/Controller/AdminPageController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class AdminPageController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin_page')]
     public function index(): Response
